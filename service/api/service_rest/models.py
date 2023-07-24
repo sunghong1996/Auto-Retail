1308eacc6e77fd2c9c1017542bb37920aea1a8cf
Zachary <zachceniceros@gmail.com>
2023-07-24 15:09:26 -0400

    created necessary models for service app

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Technician(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.PositiveSmallIntegerField(unique=True)
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    vin = models.CharField(max_length=17)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technician,
+        related_name="appointments",
+        on_delete=models.CASCADE,
+    )
