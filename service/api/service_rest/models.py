293777296425f2af73296fae017077c3367f9cc6
Zachary <zachceniceros@gmail.com>
2023-07-24 16:28:34 -0400

    got poller up and running for services

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -6,9 +6,15 @@ class Technician(models.Model):
     last_name = models.CharField(max_length=200)
     employee_id = models.PositiveSmallIntegerField(unique=True)
 
+    def __str__(self):
+        return self.first_name
+
 class AutomobileVO(models.Model):
     vin = models.CharField(max_length=17, unique=True)
     sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
 
 class Appointment(models.Model):
     date_time = models.DateTimeField()
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from .models import Technician, AutomobileVO, Appointment
+from common.json import ModelEncoder
+from django.http import JsonResponse
+import json
 
 # Create your views here.
